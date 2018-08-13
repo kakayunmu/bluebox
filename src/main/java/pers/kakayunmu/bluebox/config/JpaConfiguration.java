d37d928502449b7ed1b7e32d82f5f3221d6460d1
kakayunmu <kakayunmu@gmail.com>
2018-08-13 15:05:04 +0800

    数据库初始化时用户表增加管理员用户

--- src/main/java/pers/kakayunmu/bluebox/config/JpaConfiguration.java
+++ src/main/java/pers/kakayunmu/bluebox/config/JpaConfiguration.java
@@ -1,5 +1,8 @@
 package pers.kakayunmu.bluebox.config;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,32 +11,31 @@ import org.springframework.core.annotation.Order;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import pers.kakayunmu.bluebox.entity.Member;
+import pers.kakayunmu.bluebox.repositorys.MemberRepository;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true)
 @EnableJpaRepositories(basePackages = "pers.kakayunmu.**.repositorys")
 @EntityScan(basePackages = "pers.kakayunmu.**.entity")
+@Slf4j
 public class JpaConfiguration {
     @Bean
     PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor(){
         return  new PersistenceExceptionTranslationPostProcessor();
     }
-
-//    @Bean
-//    public CommandLineRunner FamilyInit(FamilyRepository repository) {
-//        return (args) -> {
-//            log.info("Family 初始化数据");
-//            repository.save(new Family("test"));
-//        };
-//    }
-//    @Bean
-//    public CommandLineRunner MemberInit(MemberRepository repository) {
-//        return (args) -> {
-//            log.info("Family 初始化数据");
-//            Member member=new Member();
-//            member.setName("张三");
-//            repository.save(member);
-//        };
-//    }
+    @Bean
+    public CommandLineRunner MemberInit(MemberRepository repository) {
+        return (args) -> {
+            long count = repository.count();
+            if(count==0l){
+                log.info("用户表初始化，创建管理员");
+                Member member=new Member();
+                member.setName("系统");
+                member.setOpenid("SYSTEM");
+                repository.save(member);
+            }
+        };
+    }
 }
